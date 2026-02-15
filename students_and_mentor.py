afb6bd2f81168e3712dc22f340fb7e9c0007c300
smaildoc52 <smaildoc52@gmail.com>
2026-02-15 15:17:42 +0300

    task-2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -11,6 +11,17 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
         
+    def rate_lecture(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer) and 
+                course in self.courses_in_progress and 
+                course in lecturer.courses_attached and 1 <= grade <= 10):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
+        
 
 class Mentor:
     
@@ -19,32 +30,26 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return "Ошибка"
-        
 
 class Lecturer(Mentor):
     
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
            
     
 class Reviewer(Mentor):
     
     def __init__(self, name, surname):
         super().__init__(name, surname)
-    
-    
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
-            
+        
+    def rate_hw(self, student, course, grade):
+        if (isinstance(student, Student) and 
+                course in self.courses_attached and 
+                course in student.courses_in_progress and 1 <= grade <= 10):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return "Ошибка"
